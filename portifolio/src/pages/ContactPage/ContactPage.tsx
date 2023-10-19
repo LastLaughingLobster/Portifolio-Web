be0fd4d600b8e82e1857a880e857bc3619047e7f
Henrique Cruz <henriquecruz_neto@icloud.com>
2023-10-18 21:07:43 -0300

    Adding email lambda integration

--- portifolio/src/pages/ContactPage/ContactPage.tsx
+++ portifolio/src/pages/ContactPage/ContactPage.tsx
@@ -1,27 +1,40 @@
-// Contact.tsx
-
 import React from 'react';
 import './ContactPage.css';
 import SocialButton from '../../components/SocialButton/SocialButton';
-import emailjs from 'emailjs-com';
 
 const ContactPage: React.FC = () => {
-    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
 
-      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.currentTarget, 'YOUR_USER_ID')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+        const formData = new FormData(event.currentTarget);
+        const data = {
+            name: formData.get("name") as string,
+            email: formData.get("email") as string,
+            subject: formData.get("subject") as string,
+            message: formData.get("message") as string
+        };
+    
+        try {
+            const response = await fetch('https://me7tkkvnqk.execute-api.eu-west-1.amazonaws.com/prod', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error("Error sending email:", error);
+        }
     };
 
     return (
         <div className="center-container">
             <div className="contact-container">
                 <h2 className='header-class'>Let's Work Together</h2>
-                <form className="contact-form">
+                <form className="contact-form" onSubmit={handleSubmit}>
                     <input type="text" placeholder="Name" />
                     <input type="email" placeholder="Email" />
                     <input type="text" placeholder="Subject" />
